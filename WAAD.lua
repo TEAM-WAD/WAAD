49250aa83741d24c30680413ffcd60612a7b2675
TEAM-WAD <92700528+TEAM-WAD@users.noreply.github.com>
2021-11-16 04:28:45 -0800

    Update WAAD.lua

--- WAAD.lua
+++ WAAD.lua
@@ -9801,6 +9801,7 @@ local Text ='*‹ تم اختيار بصـمةه صوتيه لك استمتع ع
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '‹ ѕᴏụʀᴄᴇ : ᴡᴀᴀᴅ ›.',url="t.me/nnnnBn"}},
+{{text = '‹ ѕᴏụʀᴄᴇ : ᴡᴀᴀᴅ ›.',url="t.me/nnnnBn"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice='..URL.escape(audios.info)..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
