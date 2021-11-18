c4396cae7c3d1011355028a6e044b433b501efdc
TEAM-WAD <92700528+TEAM-WAD@users.noreply.github.com>
2021-11-18 06:13:22 -0800

    Update WAAD.lua

--- WAAD.lua
+++ WAAD.lua
@@ -9812,14 +9812,14 @@ end
 end
 end
 if text == '/aa' or text == '/ali' or text == '/start' or text == '/pop' then  
-Text = "*ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sᴏᴜʀᴄᴇ ᴡᴀᴀᴅ*\n*⌗ : مـࢪحبـا بـك عزيزي*\n*⌗ : ﭑﻧـا بوت حمايـه ؏ـملي ﮪو حمايه الڪࢪوبات من المخࢪبين .*\n*⌗ : اذا اردت تفـ؏ـيلي اتبـ؏ الخطوات التاليه فقط .*\n*⌗ : اولا اضفني مشࢪف في الڪࢪوب .*\n*⌗ : ثانيا اࢪسل ڪلمه ( تفعيل ) في الڪروب *\n*⌗ : تلقائيا سيتم ࢪفع(الادمنيه,والمنشئين,والمالك) في الڪروب .*\n*⌗ : اࢪسل بعدها الاوامر لتحڪم باعدادات البوت  .*\n*⌗ : اذا اࢪدت لعب بالعاب التسليه في خاص البوت اࢪسل او اضغط PlaYinG/  .*"
+Text = "*ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sᴏᴜʀᴄᴇ ᴡᴀᴀᴅ*\n*⌗ : مـࢪحبـا بـك عزيزي*\n*⌗ : ﭑﻧـا بوت حمايـه ؏ـملي ﮪو حمايه الڪࢪوبات من المخࢪبين .*\n*⌗ : اذا اردت تفـ؏ـيلي اتبـ؏ الخطوات التاليه .*\n*⌗ : اولا اضفني مشࢪف في الڪࢪوب .*\n*⌗ : ثانيا اࢪسل ڪلمه ( تفعيل ) في الڪروب *\n*⌗ : تلقائيا سيتم ࢪفع(الادمنيه,والمنشئين,والمالك) في الڪروب .*\n*⌗ : اࢪسل بعدها الاوامر لتحڪم باعدادات البوت  .*\n*⌗ : اذا اࢪدت لعب بالعاب التسليه في خاص البوت اࢪسل او اضغط PlaYinG/  .*"
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '⌗ : اضفني لڪࢪوبك  .',url="http://t.me/test118bot?startgroup=new"}},
 {{text = '⌗ : ﻤـطوࢪ البوت  .',url="http://t.me/"},{text = '⌗ : الـسوࢪس  . ',url="t.me/nnnnbn"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/nnnnbn&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/nnnnBn1&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
 if text == "شنو رئيك بهذا" or text == "شنو رئيك بهذ" then
 if not database:get(bot_id..'lock:add'..msg.chat_id_) then
