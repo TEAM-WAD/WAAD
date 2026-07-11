bb2f6c5ac97a33ef847cc73282a048e861a02b53
TEAM-WAD <92700528+TEAM-WAD@users.noreply.github.com>
2026-07-11 13:29:58 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById("sideMenu");
-  menu.style.left = menu.style.left === "0px" ? "-250px" : "0px";
-}
-
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-}
